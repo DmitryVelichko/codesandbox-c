4957f60b48b35dcfda9e41f13c81c88430fe142c
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-23 23:55:01 +0800

    Linter Update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -30,7 +30,7 @@ export const Contact = (props) => {
   //       }
   //     );
   // };
-  
+
   return (
     <div>
       <div id='contact'>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -49,7 +49,7 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href='#portfolio' className='page-scroll'>
-              Expertise
+                Expertise
               </a>
             </li>
             <li>
